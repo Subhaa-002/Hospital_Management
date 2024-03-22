3b9328a9b2d6df3fbb988e77ce6098f9cb227622
Subhaa-002 <105572872+Subhaa-002@users.noreply.github.com>
2024-03-22 09:50:58 +0530

    Update HospitalService.py

--- Python_CodingChallenge/dao/HospitalService.py
+++ Python_CodingChallenge/dao/HospitalService.py
@@ -2,26 +2,6 @@ from entity.Appointment import Appointment
 from util.DBConnection import DBConnection
 from exception.Exception import PatientNumberNotFoundException,AppointmentNotFoundException
 
-class IHospitalService:
-    def get_appointment_by_id(self, appointmentId):
-        pass
-
-    def get_appointments_for_patient(self, patientId):
-        pass
-
-    def get_appointments_for_doctor(self, doctorId):
-        pass
-
-    def schedule_appointment(self, appointment):
-        pass
-
-    def update_appointment(self, appointment):
-        pass
-
-    def cancel_appointment(self, appointmentId):
-        pass
-
-
 class HospitalServiceImpl(IHospitalService):
 
     def get_appointment_by_id(self, appointmentId):
