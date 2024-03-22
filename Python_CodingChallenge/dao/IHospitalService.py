42984cbdd127d6cc9c15a12e179c40d8d638417e
Subhaa-002 <105572872+Subhaa-002@users.noreply.github.com>
2024-03-22 09:51:44 +0530

    Create IHospitalService.py

---
+++ Python_CodingChallenge/dao/IHospitalService.py
@@ -0,0 +1,18 @@
+class IHospitalService:
+    def get_appointment_by_id(self, appointmentId):
+        pass
+
+    def get_appointments_for_patient(self, patientId):
+        pass
+
+    def get_appointments_for_doctor(self, doctorId):
+        pass
+
+    def schedule_appointment(self, appointment):
+        pass
+
+    def update_appointment(self, appointment):
+        pass
+
+    def cancel_appointment(self, appointmentId):
+        pass
